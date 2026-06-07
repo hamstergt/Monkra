e11b9c2dac2688fc64313860e7a07544c8b1ec14
hamstergt <reubenplaysgames123@gmail.com>
2026-06-07 14:04:31 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Moderating Monkra",
+        name: "Moderating Monkra And THE HamsterGT Community",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
